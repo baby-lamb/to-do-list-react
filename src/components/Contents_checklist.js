4c5c4d58889552785c42ee15fdd66009dbdd3869
Pam <59804940+Naju-Baek@users.noreply.github.com>
2020-02-25 23:50:35 +0900

    달력 만들기

--- src/components/Contents_checklist.js
+++ src/components/Contents_checklist.js
@@ -21,6 +21,15 @@ class Contents_checklist extends Component {
           <li>FRI</li>
           <li>SAT</li>
         </ul>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <li>6</li>
+          <li>7</li>
+        </ul>
       </section>
     );
   }
