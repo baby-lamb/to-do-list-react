9ac30ed06851b767306502a517dbd459ebfde8d2
Pam <59804940+Naju-Baek@users.noreply.github.com>
2020-03-09 20:14:18 +0900

    달력 만들기 7

--- src/components/Contents_checklist.js
+++ src/components/Contents_checklist.js
@@ -2,6 +2,32 @@ import React, { Component } from 'react';
 import './Contents_checklist.css';
 
 
+function Calender(){
+  function generate(){
+    const today = moment();
+    const startWeek = today.clone().startOf('month').week();
+    const endWeek = today.clone().endOf('month').week(); === 1 ? 53 : today.clone().endOf('month').week();
+    let calender = [];
+    for (let week = startWeek; week <= endWeek; week++){
+      calender.push(
+        <div className="row" key={week}>
+          {
+            Array(7).fill(0).map((n,i) => {
+              let current = today.clone().week(week).startOf('week').add(n + i, 'day')
+              let isSelected = today.format('YYYYMMDD') === current.format('YYYYMMDD') ? 'selected' : '';
+              let isGrayed = current.format('MM') === today.format('MM') ? '' : 'grayed';
+              return (
+                <div className={`box  ${isSelected} ${isGrayed}`} key={i}>
+                  <span className={`text`}>{current.format('D')}</span>
+                </div>
+              )
+            })
+          }
+        </div>
+      )
+    }
+  return calender;
+}
 
 class Contents_checklist extends Component {
   render() {
