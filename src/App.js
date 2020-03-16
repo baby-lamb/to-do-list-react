46391dd4e6b11e96481192b89e09436b498fe26d
Pam <59804940+Naju-Baek@users.noreply.github.com>
2020-03-16 22:09:05 +0900

    오류 수정

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import MainHeader from './components/MainHeader';
 import ContentsTodolist from './components/ContentsTodolist';
-// import ContentsChecklist from './components/ContentsChecklist';
-// import ContentsCalender from './components/ContentsCalender';
+import ContentsChecklist from './components/ContentsChecklist';
+import ContentsCalender from './components/ContentsCalender';
 
 class App extends Component {
   constructor(props){
@@ -22,12 +22,13 @@ class App extends Component {
       <div className="App">
         <MainHeader data={this.state.contents}></MainHeader>
         <ContentsTodolist></ContentsTodolist>
+        <ContentsChecklist></ContentsChecklist>
+        <ContentsCalender></ContentsCalender>
       </div>
     );
   }
 }
 
-// <ContentsChecklist></ContentsChecklist>
-// <ContentsCalender></ContentsCalender>
+
 
 export default App;
